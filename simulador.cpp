6323a442f8d29175c9b22b97171c2350b02b8c5a
BawdenS <50454082+BawdenS@users.noreply.github.com>
2019-10-20 17:26:35 -0300

    Add files via upload
    SIMULADOR FEITO

---
+++ simulador.cpp
@@ -0,0 +1,129 @@
+// Simulator.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using namespace std;
+int main(int Nargumentos, char*Argumentos[])
+{
+	int i, Auxiliar,Acumulador = 0;
+
+	string NomeArquivo = Argumentos[1];
+	string Linha, Word;
+	vector<int> Unidade;
+	ifstream Leitura;
+
+	Leitura.open(NomeArquivo.c_str());
+	if (!Leitura.is_open()) 
+	{
+		cerr << " Nao eh possivel abrir arquivo" << endl;
+		exit(1);
+	}
+
+	while (getline(Leitura,Linha)) 
+	{
+		istringstream iss(Linha);
+		while (iss >> Word) 
+		{
+			Unidade.push_back(stoi(Word));
+		}
+	}
+	for (i = 0; i < Unidade.size();i++) {
+		//ADD
+		if (Unidade.at(i) == 1){
+			Acumulador += Unidade.at(Unidade.at(i + 1));
+			i++;
+			cout << "Acumulador: " << Acumulador << endl;
+		}
+		//SUB
+		if (Unidade.at(i) == 2) {
+			Acumulador -= Unidade.at(Unidade.at(i + 1));
+			i++;
+			cout << "Acumulador: " << Acumulador << endl;
+		}
+		//MULT
+		if (Unidade.at(i) == 3) {
+			Acumulador *= Unidade.at(Unidade.at(i + 1));
+			i++;
+			cout << "Acumulador: " << Acumulador << endl;
+		}
+		//DIV
+		if (Unidade.at(i) == 4) {
+			Acumulador /= Unidade.at(Unidade.at(i + 1));
+			i++;
+			cout << "Acumulador: " << Acumulador << endl;
+		}
+		//JMP
+		if (Unidade.at(i) == 5) {
+			i = Unidade.at(i++);
+		}
+		//JMPN
+		if (Unidade.at(i) == 6) {
+			if (Acumulador < 0) {
+				i = Unidade.at(i++);
+			}
+			else 
+			{
+				i++;
+			}
+		}
+		//JMPP
+		if (Unidade.at(i) == 7) {
+			if (Acumulador > 0) {
+				i = Unidade.at(i++);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		//JMPZ
+		if (Unidade.at(i) == 8) {
+			if (Acumulador == 0) {
+				i = Unidade.at(i++);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		//COPY
+		if (Unidade.at(i) == 9) {
+			Unidade.at(Unidade.at(i + 2)) = Unidade.at(Unidade.at(i + 1));
+			i++;
+			i++;
+		}
+		//LOAD
+		if (Unidade.at(i) == 10) {
+			Acumulador = Unidade.at(Unidade.at(i + 1));
+			i++;
+			cout << "Acumulador: "<< Acumulador << endl;
+		}
+		//STORE
+		if (Unidade.at(i) == 11) {
+			Unidade.at(Unidade.at(i + 1)) = Acumulador;
+			cout << "Memoria em: "<< Unidade.at(i + 1) << " Alterada para: " << Acumulador << endl;
+		}
+		//INPUT
+		if (Unidade.at(i) == 12) {
+			cout << "Favor Inserir Valor numerico: "<< endl;
+			cin >> Unidade.at(Unidade.at(i + 1));
+			i++;
+		}
+		//OUTPUT
+		if (Unidade.at(i) == 13) {
+			cout << "O Valor de Saida eh:" << Unidade.at(Unidade.at(i + 1)) << endl;
+			i++;
+		}
+		//STOP
+		if (Unidade.at(i) == 14) {
+			cout << "Fim do programa" <<endl;
+			return 0;
+		}
+	}
+	Leitura.close();
+	return 0;
+}
